d0a6ff89defc7a8f0d7da0561ba132483ae54d4f
felix gustafsson <felix@pixotag.com>
2017-11-14 20:56:11 +0100

    ett test med matlab

---
+++ Testtenta_3.m
@@ -0,0 +1,20 @@
+%Derivering
+
+data = load('smogenSpring2010.txt');
+t = data(:,1);
+h = data(:,2);
+
+t=t*(60*60*24);
+h=h/1000;
+
+
+q =57000000*(diff(h)/diff(t));
+
+
+figure(1)
+plot(q)
+grid on
+t=t/(60*60*24);
+xticks(0:size(t)/10:size(t))
+xticklabels(round(t(1)):10:t(end))
+%yticks(0:10:100)
